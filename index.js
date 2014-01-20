7db0dca21355a46b6d3e8329f63f22b71c975cb4
Jon Schlinkert <dev@sellside.com>
2014-01-20 17:56:52 -0500

    improve defaults, language detection in particular.

--- index.js
+++ index.js
@@ -13,66 +13,73 @@ var marked   = require('marked');
 var renderer = new marked.Renderer();
 var _        = require('lodash');
 
+
 // Mix in the methods from underscore string
 _.mixin(require('underscore.string'));
 
-// Initialize custom language settings for highlight.js
-hljs.registerLanguage('less', require('./lib/less.js'));
-
-
 module.exports.init = function(options) {
-  options = options || {};
 
-  renderer.heading = function (text, level) {
-    var fallbackTemplate = './lib/tmpl.js';
-    var output, heading = options.heading;
-    if (heading && _.isString(heading) && heading.length > 0) {
-      output = file.readFileSync(options.heading);
-    } else if (_.isFunction(heading)) {
-      output = require(heading);
-    } else {output = require(fallbackTemplate);}
-    return _.template(output, {
-      text: text,
-      name: _.slugify(text),
-      level: level
-    });
+  var languages = exports.languages = {
+    coffee    : 'coffeescript',
+    handlebars: 'handlebars',
+    hbs       : 'handlebars',
+    html      : 'xml',
+    xml       : 'xml',
+    javascript: 'javascript',
+    js        : 'javascript',
+    json      : 'javascript',
+    less      : 'less',
+    markdown  : 'markdown',
+    md        : 'markdown',
+    pl        : 'perl',
+    rb        : 'ruby',
+    ru        : 'ruby',
+    yaml      : 'yaml',
+    yfm       : 'yaml',
+    yml       : 'yaml',
   };
-  exports.matchesExt = function (haystack, needles) {
-    needles = Array.isArray(needles) ? needles : [needles];
-    needles = (needles.length > 0) ? _.unique(_.flatten(needles)).join('|') : '.*';
-    var re = new RegExp('(?:' + needles + ')', 'g');
-    var matches = String(haystack).match(re);
-    if (matches) {
-      return true;
-    } else {
-      return false;
+
+  // See ./lib/tmpl.js
+  renderer.heading = function (text, level) {
+    var output;
+    if(options.heading && options.heading.length > 0) {
+      output = file.readFileSync(options.heading);
+      // Use fallback heading template when one isn't defined in options
+    } else {output = require('./lib/tmpl.js');}
+    return _.template(output, {text: text, level: level});
+  };
+
+  hljs.configure({
+    tabReplace: ' ',
+    classPrefix: options.prefix || 'language-'
+  });
+
+  // Initialize custom language settings for highlight.js
+  hljs.registerLanguage('less', require('./lib/less.js'));
+
+  var defaults = {
+    renderer: renderer,
+    gfm: true,
+    tables: true,
+    breaks: false,
+    pedantic: false,
+    sanitize: false,
+    smartLists: true,
+    smartypants: false,
+    highlight: function (code, lang) {
+      if (lang === undefined) {lang = 'bash';}
+      Object.keys(languages).forEach(function (language) {
+        lang = languages[language];
+      });
+      try {
+        return hljs.highlight(lang, code).value;
+      } catch (e){
+        return hljs.highlightAuto(code).value;
+      }
     }
   };
 
-  exports.languages = {
-    xml: ['html', 'xml'],
-    less: ['less'],
-    yaml: ['yfm', 'yml', 'yaml'],
-    markdown: ['markdown', 'md'],
-    handlebars: ['handlebars', 'hbs'],
-    javascript: ['javascript', 'js', 'json']
-  };
-
-  exports.markedDefaults = {
-    sanitize: false,
-    highlight: function (code, lang) {
-      if (lang === undefined) {lang = 'bash';}
-      for (var language in exports.languages) {
-        if (exports.languages.hasOwnProperty(language)) {
-          if (exports.matchesExt(lang, exports.languages[language])) {
-            lang = language;
-          }
-        }
-      }
-      return hljs.highlight(lang, code).value;
-    },
-    renderer: renderer
-  };
+  exports.markedDefaults = _.extend(defaults, options);
 
   return exports;
 };
